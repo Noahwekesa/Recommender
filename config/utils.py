009d207718d2106403bfd3875bf65047de912a5a
noahwekesa <noahwekesa0@gmail.com>
2024-07-12 17:16:53 +0300

    generate fake users with fname and lname

--- config/utils.py
+++ config/utils.py
@@ -1,5 +1,23 @@
 from faker import Faker
 
-faker = Faker()
 
-faker.profile()
+def get_fake_profiles(count=10):
+    """
+    generate fake profile
+    """
+    fake = Faker()
+    user_data = []
+    for _ in range(count):
+        profile = fake.profile()
+        data = {
+            "username": profile.get("username"),
+            "email": profile.get("mail"),
+            # "password": make_password(fake.password(length=15)),
+            "is_true": True,
+        }
+        if "name" in profile:
+            fname, lname = profile.get("name").split(" ")[:2]
+            data["first_name"] = fname
+            data["last_name"] = lname
+        user_data.append(data)
+    return user_data

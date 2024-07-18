731ff338faa93ac20401b0fc20f9cc6ae62fc3aa
noahwekesa <noahwekesa0@gmail.com>
2024-07-18 18:36:23 +0300

    calculate the ratings avg and generate the ratings

--- movies/models.py
+++ movies/models.py
@@ -1,10 +1,28 @@
 import datetime
 from django.contrib.contenttypes.fields import GenericRelation
+from django.db.models import Q
 from django.db import models
 from django.utils import timezone
 from ratings.models import Rating
 
-RATING_CALC_TIME = 1
+RATING_CALC_TIME_IN_DAYS = 3
+
+
+class MovieQuerySet(models.QuerySet):
+    def needs_updating(self):
+        now = timezone.now()
+        days_ago = now - datetime.timedelta(days=RATING_CALC_TIME_IN_DAYS)
+        return self.filter(
+            Q(rating_last_updated__isnull=True) | Q(rating_last_updated__lte=days_ago)
+        )
+
+
+class MovieManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return MovieQuerySet(self.model, using=self._db)
+
+    def needs_updating(self):
+        return self.get_queryset().needs_updating()
 
 
 class Movie(models.Model):
@@ -35,6 +53,7 @@ class Movie(models.Model):
         null=True,
         blank=True,
     )
+    objects = MovieManager()
 
     def __str__(self):
         if not self.release_date:
@@ -46,7 +65,7 @@ class Movie(models.Model):
         if not self.rating_last_updated:
             return self.calculate_rating()
         if self.rating_last_updated > now - datetime.timedelta(
-            minutes=RATING_CALC_TIME
+            days=RATING_CALC_TIME_IN_DAYS
         ):
             return self.rating_avg
         return self.calculate_rating()
